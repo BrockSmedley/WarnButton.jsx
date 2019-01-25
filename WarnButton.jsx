afaede9ec3992e639f38b0af156b9d9cca4739b8
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-01-25 00:22:21 -0800

    accept children as primary content
    + add default vars

--- WarnButton.jsx
+++ WarnButton.jsx
@@ -4,43 +4,45 @@ import { Button } from 'react-materialize';
 class WarnButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  
-            color: props.color || "green",
+        let defaultColor = "green", defaultAltColor = "red";
+        let defaultContent = "delete", defaultAltContent = "delete?";
+        this.state = {
+            color: props.color || defaultColor,
             warn: false,
-            content: props.content || "Delete"
+            content: props.children || defaultContent
         };
-        
+
         this.statePresets = [
             {
-                color: this.props.color || "green",
-                content: this.props.content || "Delete"
+                color: this.props.color || defaultColor,
+                content: this.props.children || defaultContent
             },
             {
-                color: this.props.altColor || "red",
-                content: this.props.altContent || this.state.content || "Delete"
+                color: this.props.altColor || defaultAltColor,
+                content: this.props.altContent || this.props.children || defaultAltContent
             }
         ];
 
         this.toggleState = this.toggleState.bind(this);
         this.resetState = this.resetState.bind(this);
     }
-    toggleState(e){
+    toggleState(e) {
         if (this.state.warn)
             this.props.action(e);
-        this.setState({ warn: !this.state.warn, ...this.statePresets[this.state.warn ? 0 : 1]});
-        
+        this.setState({ warn: !this.state.warn, ...this.statePresets[this.state.warn ? 0 : 1] });
+
     }
-    resetState(e){
-        this.setState({ warn: false, ...this.statePresets[0]});
+    resetState(e) {
+        this.setState({ warn: false, ...this.statePresets[0] });
     }
     render() {
         return (
-            <Button {...this.props.buttonProps} 
+            <Button {...this.props.buttonProps}
                 flat={this.state.warn}
-                className={this.state.color} 
+                className={this.state.color}
                 onClick={this.toggleState}
                 onBlur={this.resetState}
-                >
+            >
                 {this.state.content}
             </Button>
         );
